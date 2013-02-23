53b91f10a3ceb5ceec652d5639caa0c1587c7653
Kaustuv Chaudhuri <kaustuv.chaudhuri@inria.fr>
2013-02-23 23:24:10 +0100

    Finishing work on actions; it is now sealed and independent of the gui

--- action.ml
+++ action.ml
@@ -9,11 +9,10 @@ open Syntax
 open Result
 
 type mmode =
-  | No_marks
-  | Marked of Traversal.trail
+  | Unmarked
+  | Marked
 
 type snap = {
-  trail : int list ;
   mmode : mmode ;
   form : form ;
 }
@@ -31,10 +30,23 @@ type history = {
 let init f =
   let cur = {
     form = f ;
-    trail = [] ;
-    mmode = No_marks ;
+    mmode = Unmarked ;
   } in
   { work = cur ; dirty = false ; past = [] ; present = cur ; future = [] }
+
+let strip_mmode snap = snap.form
+let render hi =
+  let past = List.map strip_mmode hi.past in
+  let cur  = strip_mmode hi.work in
+  let fut  = List.map strip_mmode hi.future in
+  (past, cur, fut)
+
+type action_error =
+  | Parsing_witness
+exception Action_failure of action_error
+let actfail err = raise (Action_failure err)
+let explain = function
+  | Parsing_witness -> "cannot parse term"
 
 let tinker ~fn hi =
   try 
@@ -43,6 +55,7 @@ let tinker ~fn hi =
   with
   | Traversal.Traversal_failure err -> Bad (Traversal.explain err)
   | Rules.Rule_failure err -> Bad (Rules.explain err)
+  | Action_failure err -> Bad (explain err)
 
 let commit ~fn hi =
   try
@@ -52,14 +65,12 @@ let commit ~fn hi =
   with
   | Traversal.Traversal_failure err -> Bad (Traversal.explain err)
   | Rules.Rule_failure err -> Bad (Rules.explain err)
-
+  | Action_failure err -> Bad (explain err)
 
 type action = {
   enabled : history -> bool ;
   perform : history -> (history, string) Result.t
 }
-
-(* actions *)
 
 let action_undo = {
   enabled = (fun hi -> hi.dirty || hi.past <> []) ;
@@ -103,8 +114,7 @@ let action_descend = {
     tinker hi ~fn:begin
       fun snap ->
         let form = Traversal.go_down 0 snap.form in
-        let trail = 0 :: snap.trail in
-        { snap with trail ; form }
+        { snap with form }
     end
   end }
 
@@ -114,8 +124,7 @@ let action_ascend = {
     tinker hi ~fn:begin
       fun snap -> 
         let form = Traversal.go_up snap.form in
-        let trail = List.tl snap.trail in
-        { snap with trail ; form }
+        { snap with form }
     end
   end }
 
@@ -125,8 +134,7 @@ let action_ascend_to_top = {
     tinker hi ~fn:begin
       fun snap ->
         let form = Traversal.go_top snap.form in
-        let trail = [] in
-        { snap with trail ; form }
+        { snap with form }
     end
   end }
 
@@ -136,8 +144,7 @@ let action_left = {
     tinker hi ~fn:begin
       fun snap ->
         let form = Traversal.go_left snap.form in
-        let trail = (List.hd snap.trail - 1) :: List.tl snap.trail in
-        { snap with trail ; form }
+        { snap with form }
     end
   end }
 
@@ -147,27 +154,30 @@ let action_right = {
     tinker hi ~fn:begin
       fun snap ->
         let form = Traversal.go_right snap.form in
-        let trail = (List.hd snap.trail + 1) :: List.tl snap.trail in
-        { snap with trail ; form }
+        { snap with form }
     end
   end }
 
 let action_mark_source = {
-  enabled = (fun hi -> hi.work.mmode = No_marks) ;
+  enabled = (fun hi -> hi.work.mmode = Unmarked) ;
   perform = begin fun hi ->
     tinker hi ~fn:begin
       fun snap ->
         let form = Rules.make_lnk SRC snap.form in
-        let mmode = Marked snap.trail in
-        { snap with form ; mmode }
+        let mmode = Marked in
+        { form ; mmode }
     end
   end }
 
 let action_unmark_source = {
-  enabled = (fun hi ->
-    match hi.work.mmode with
-    | No_marks -> false
-    | Marked trail -> trail = hi.work.trail ) ;
+  enabled = begin fun hi ->
+    hi.work.mmode = Marked && begin
+      let (_, f) = unsubst hi.work.form in
+      match f with
+      | Conn (Mark SRC, _) -> true
+      | _ -> false
+    end
+  end ;
   perform = begin fun hi ->
     tinker hi ~fn:begin
       fun snap ->
@@ -175,12 +185,137 @@ let action_unmark_source = {
         match f with
         | Conn (Mark SRC, [f]) ->
             let form = subst fcx f in
-            let mmode = No_marks in
-            { snap with form ; mmode }
+            let mmode = Unmarked in
+            { form ; mmode }
         | _ ->
             assert false
     end
   end }
+
+let action_zero = {
+  enabled = begin fun hi ->
+    let (_, f) = unsubst hi.work.form in
+    match f with
+    | Conn (Plus, []) -> false
+    | _ -> not (Rules.has_lnk f)
+  end ;
+  perform = begin fun hi ->
+    commit hi ~fn:begin
+      fun snap ->
+        let (fcx, _) = unsubst snap.form in
+        let form = subst fcx _Zero in
+        { snap with form }
+    end
+  end }
+
+let action_weaken = {
+  enabled = begin fun hi ->
+    let (_, f) = unsubst hi.work.form in
+    match f with
+    | Conn (Qm, [f]) -> not (Rules.has_lnk f)
+    | _ -> false
+  end ;
+  perform = begin fun hi ->
+    commit hi ~fn:begin
+      fun snap ->
+        let (fcx, _) = unsubst snap.form in
+        let form = subst fcx _Bot in
+        { snap with form }
+    end
+  end }
+
+let action_derelict = {
+  enabled = begin fun hi ->
+    let (_, f) = unsubst hi.work.form in
+    match f with
+    | Conn (Qm, [_]) -> true
+    | _ -> false
+  end ;
+  perform = begin fun hi ->
+    commit hi ~fn:begin
+      fun snap ->
+        let (fcx, form) = unsubst snap.form in
+        match form with
+        | Conn (Qm, [form]) ->
+            let form = subst fcx form in
+            { snap with form }
+        | _ -> assert false
+    end
+  end }
+
+let action_contract = {
+  enabled = begin fun hi ->
+    let (_, f) = unsubst hi.work.form in
+    match f with
+    | Conn (Qm, [f]) -> not (Rules.has_lnk f)
+    | _ -> false
+  end ;
+  perform = begin fun hi ->
+    commit hi ~fn:begin
+      fun snap ->
+        let (fcx, form) = unsubst snap.form in
+        let (fcx, fr) =
+          begin match Cx.rear fcx with
+          | Some (fcx, ({fconn = PAR ; fright ; _} as fr)) ->
+              (fcx, {fr with fright = form :: fright})
+          | _ ->
+              (fcx, {fconn = PAR ; fleft = [] ; fright = [form]})
+          end in
+        let fcx = Cx.snoc fcx fr in
+        let form = subst fcx form in
+        { snap with form }
+    end
+  end }
+
+let action_subst ~read = {
+  enabled = begin fun hi ->
+    let (_, f) = unsubst hi.work.form in
+    match f with
+    | Conn (Ex _, _) -> true
+    | _ -> false
+  end ;
+  perform = begin fun hi ->
+    commit hi ~fn:begin
+      fun snap ->
+        let (fcx, form) = unsubst snap.form in
+        match form with
+        | Conn (Ex x, [fb]) ->
+            begin match read x (fcx_vars fcx) with
+            | Some t ->
+                let ss = Dot (Shift 0, t)in
+                let form = subst fcx (sub_form ss fb) in
+                { snap with form }
+            | None ->
+                actfail Parsing_witness
+            end
+        | _ -> assert false
+    end
+  end }
+
+let action_complete_link = {
+  enabled = begin fun hi ->
+    hi.work.mmode = Marked
+  end ;
+  perform = begin fun hi ->
+    commit hi ~fn:begin
+      fun snap ->
+        let form = Rules.make_lnk SNK snap.form in
+        let form = Rules.resolve_mpar form in
+        { mmode = Unmarked ; form }
+    end
+  end }
+
+let action_reset = {
+  enabled = (fun _ -> true) ;
+  perform = begin fun hi ->
+    tinker hi ~fn:begin
+      fun snap ->
+        let form = Traversal.cleanup snap.form in
+        { mmode = Unmarked ; form }
+    end
+  end }
+
+(*
 
 (* key processing *)
 
@@ -250,3 +385,5 @@ let handle_key (kmap : kmap) key hi =
       in
       scan ads
   end
+
+*)
---
+++ action.mli
@@ -0,0 +1,36 @@
+(******************************************************************************)
+(* Author: Kaustuv Chaudhuri <kaustuv.chaudhuri@inria.fr>                     *)
+(* Copyright (C) 2013  INRIA                                                  *)
+(* See LICENSE for licensing details.                                         *)
+(******************************************************************************)
+
+open Batteries
+open Syntax
+
+type history
+
+val init : form -> history
+
+val render : history -> form list * form * form list
+
+type action = {
+  enabled : history -> bool ;
+  perform : history -> (history, string) Result.t ;
+}
+
+val action_undo : action
+val action_redo : action
+val action_descend : action
+val action_ascend : action
+val action_ascend_to_top : action
+val action_left : action
+val action_right : action
+val action_mark_source : action
+val action_unmark_source : action
+val action_zero : action
+val action_weaken : action
+val action_derelict : action
+val action_contract : action
+val action_subst : read:(Idt.t -> Idt.t list -> term option) -> action
+val action_complete_link : action
+val action_reset : action
